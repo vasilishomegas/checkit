1c3301fe2d2f3dbcf0f29ecbbd09f7da0f251f34
Gabrielius <gabrieliusb02@gmail.com>
2019-11-09 18:50:41 +0100

    Revert "Revert "First model class""
    This reverts commit bb4b39ed2c7424e8b0890d39fa79f780c542f595.

---
+++ ListIt_Backend/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class User
+{
+    private int Id { get; set; }
+    private Country Country { get; set; }
+    private Language Language { get; set; }
+    private string Nickname { get; set; }
+    private string Email { get; set; }
+    private string PasswordHash { get; set; }
+    //private string SocialMediaLogIn { get; set; }
+    private DateTime Timestamp { get; set; }
+}
