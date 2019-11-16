a4af19c3a887d59919887be97a542aee1ac0aa3d
Gabrielius <gabrieliusb02@gmail.com>
2019-11-16 05:03:53 +0100

    Made service class for each model

---
+++ ListIt_BusinessLogic/Services/CurrencyService.cs
@@ -0,0 +1,9 @@
+﻿using ListIt_BusinessLogic.Services.Generics;
+using ListIt_DomainModel;
+
+namespace ListIt_BusinessLogic.Services
+{
+    class CurrencyService : Service<Currency>
+    {
+    }
+}
