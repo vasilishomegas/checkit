581ef533d72bffba8dd881c2c5a8fe511c2a965d
nadinegostner <nadine.gostner@gmx.at>
2019-11-20 00:21:22 +0100

    fixed some design/layout issues

--- ListIt_WebFrontend/App_Start/BundleConfig.cs
+++ ListIt_WebFrontend/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace ListIt_WebFrontend
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css"
                       ));
         }
