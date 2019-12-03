0ba99fa1b8164be49e991efe35050b29abad05c2
nadinegostner <nadine.gostner@gmx.at>
2019-12-03 16:03:38 +0100

    added CurrencyRepository and Service

---
+++ ListIt_DataAccess/Repository/CurrencyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ListIt_DataAccess.Repository.Generics;
+using ListIt_DataAccessModel;
+
+namespace ListIt_DataAccess.Repository
+{
+    public class CurrencyRepository : Repository<Currency>
+    {
+
+        //TODO: get names from Translation table
+    }
+}
---
+++ ListIt_DomainModel/DTO/CurrencyDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ListIt_DomainModel.DTO.Interfaces;
+
+namespace ListIt_DomainModel.DTO
+{
+    public class CurrencyDto : IDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sign { get; set; }
+        public string Code { get; set; }
+    }
+}
