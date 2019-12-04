52c23c095ee64f62acbbcd52cbaf946d85606d0c
Willem <willembike@gmail.com>
2019-12-04 10:44:45 +0100

    Stupid VS

---
+++ ListIt_DomainModel/ChainDTO.cs
@@ -0,0 +1,12 @@
+﻿using ListIt_DomainModel.DTO.Interfaces;
+
+namespace ListIt_DomainModel.DTO
+{
+    public class ChainDto : IDto
+    {
+        public int Id { get; set; }
+        public ShopApiDto ShopApi { get; set; }
+        public string Name { get; set; }
+        public string Logo { get; set; }
+    }
+}
