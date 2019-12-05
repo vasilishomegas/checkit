12326f437c2a5e72be3ac752ba81b73ccf6c46e5
Dora Grgurev <dora.grgurev@gmail.com>
2019-12-05 16:48:10 +0100

    First steps toward admin login logic.

---
+++ ListIt_DomainModel/DTO/AdminDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ListIt_DomainModel.DTO.Interfaces;
+
+namespace ListIt_DomainModel.DTO
+{
+    public class AdminDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
