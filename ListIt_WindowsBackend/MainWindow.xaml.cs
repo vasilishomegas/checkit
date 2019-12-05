2473d170e080059919618731a4e409707581a7e0
Dora Grgurev <dora.grgurev@gmail.com>
2019-12-05 14:52:07 +0100

    Admin login interface created.

--- ListIt_WindowsBackend/MainWindow.xaml.cs
+++ ListIt_WindowsBackend/MainWindow.xaml.cs
@@ -23,8 +23,8 @@ namespace ListIt_WindowsBackend
         public MainWindow()
         {
             InitializeComponent();
-            AddProduct addProduct = new AddProduct();
-            addProduct.Show();
+            AdminLogin adminLogin = new AdminLogin();
+            adminLogin.Show();
             this.Close();
         }
     }
