92f728e6a6fefc045fe4f35483237d733c2f0fdf
patrykiciak <patryk.iciak@gmail.com>
2019-12-05 03:37:10 +0100

    base for Unit Tests, issues with inconclusive tests

---
+++ ListIt_BusinessLogic_Tests_Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ListIt_BusinessLogic_Tests_Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ListIt_BusinessLogic_Tests_Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("947c0599-399c-44e4-9585-3971464c526e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ListIt_BusinessLogic_Tests_Unit/Services/Generics/ServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListIt_BusinessLogic.Services.Generics;
+using ListIt_DataAccess.Repository.Generics;
+using Moq;
+using NUnit.Framework;
+
+namespace ListIt_BusinessLogic_Tests_Unit.Services.Generics
+{
+    // https://stackoverflow.com/questions/49659515/resharper-shows-all-tests-as-inconclusive-and-has-error-testadapter-dll-does-no
+    public class ServiceTests<T, DTO>
+        where T : class, new()
+        where DTO : class, new()
+    {
+        [Test]
+        public void GetAll_ShouldReturnTheSameNumberOfElements()
+        {
+            // Arrange
+            var entities = new List<T>() { new T { }, new T { } };
+            var repository = new Mock<IRepository<T>>();
+            repository.Setup(r => r.GetAll()).Returns(entities);
+            var converter = new Mock<IDtoDbConverter<T, DTO>>();
+            var service = new Service<T, DTO>(repository.Object, converter.Object);
+
+            // Act
+            var result = service.GetAll();
+
+            // Assert
+            Assert.Equals(entities.Count, result.Count());
+        }
+
+        [Test] 
+        public void Check()
+        {
+
+        }
+    }
+}
