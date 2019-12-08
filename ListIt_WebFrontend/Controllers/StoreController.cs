c9cfa3bb800362fa74eda44d1872a932d831a946
nadinegostner <nadine.gostner@gmx.at>
2019-12-08 16:38:16 +0100

    embedded GoogleMaps via API + Key

--- ListIt_WebFrontend/Controllers/StoreController.cs
+++ ListIt_WebFrontend/Controllers/StoreController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ListIt_WebFrontend.Models;
+using ListIt_BusinessLogic.Services;
+using ListIt_DomainModel.DTO;
 
 namespace ListIt_WebFrontend.Controllers
 {
@@ -14,11 +17,64 @@ namespace ListIt_WebFrontend.Controllers
             return View();
         }
 
+        #region FindStore
+        protected string mapsKey = "&key=AIzaSyAk3NRXmyq5nFc3iXwZMcNNp-jDNJzup1c";
+        protected string defaultUrl = "https://www.google.com/maps/embed/v1/search?q=grocery+stores+near";
+
         // GET: Store/FindStore
         public ActionResult FindStore()
         {
-            return View();
+            ViewBag.Message = TempData["SuccessMessage"];
+            ViewBag.Error = TempData["ErrorMessage"];
+
+            // https://developers.google.com/maps/documentation/urls/guide
+            /* ENCODING URLs */
+            // pipe character (|) as a separator, which you must encode as %7C in the final URL
+            // encode the comma as %2C
+            // Encode spaces with %20, or replace them with a plus sign (+).
+            //https://developers.google.com/maps/documentation/embed/usage-and-billing
+
+            //var mapsKey = "&key=AIzaSyAk3NRXmyq5nFc3iXwZMcNNp-jDNJzup1c";
+
+            FindStoreVM findStoreVM = new FindStoreVM();
+            findStoreVM.Place = "Aalborg, Denmark"; //initialized default place Aalborg
+            findStoreVM.Url = defaultUrl + "+Aalborg+Denmark" + mapsKey;
+
+            return View(findStoreVM);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoadMap(FormCollection collection)
+        {
+            try
+            {
+                var searchPlace = collection["Place"];
+                FindStoreVM findStoreVM = new FindStoreVM();
+                findStoreVM.Place = searchPlace;
+
+                char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+                string[] keywords = searchPlace.Split(delimiterChars);
+                string search = "";                
+
+                foreach (String word in keywords)
+                {
+                    search = search + "+" + word;
+                }
+
+                findStoreVM.Url = defaultUrl + search + mapsKey;
+
+                return View("FindStore", findStoreVM);
+            }
+            catch
+            {
+                TempData["ErrorMessage"] = "You search request could not be found.";
+                return RedirectToAction("FindStore");
+            }
+            
+        }
+
+        #endregion FindStore
 
         // GET: Store/BrowseStore
         public ActionResult BrowseStore()
@@ -26,76 +82,6 @@ namespace ListIt_WebFrontend.Controllers
             return View();
         }
 
-        // GET: Store/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Store/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Store/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Store/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Store/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Store/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Store/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
---
+++ ListIt_WebFrontend/models/FindStoreVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListIt_WebFrontend.Models
+{
+    public class FindStoreVM
+    {
+        public string Url { get; set; }
+        public string Place { get; set; }
+    }
+}
